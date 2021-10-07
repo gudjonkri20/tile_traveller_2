fd85a7c086d62a9708bcc43e2f247944a359eabc
Jong18 <89984416+Jong18@users.noreply.github.com>
2021-10-07 14:01:49 +0000

    Update tile_traveller_2.py

--- tile_traveller_2.py
+++ tile_traveller_2.py
@@ -1,3 +1,49 @@
+'''Add the following functionality to tiles (1,2), (2,2), (2,3) and (3,2):
+
+When the player enters these tiles offer them the choice to pull a lever.  
+If they choose to pull the lever (answering either with ‘y’ or ‘Y’/ ‘N’ or ‘n’), 
+they are to be told that they received one coin.
+
+There is no limit on how often the player can get a coin from each lever.
+ But they should only be presented with the option to pull a lever once each time they are in a particular room. 
+ So if they want to be asked again, they must first go away (to another tile) and come back. 
+ Picking an invalid direction (walking into a wall) does not count as leaving the room.
+
+In addition, you need to keep track of the number of coins the player has and display that before showing the 
+available actions.
+
+When a victory has been reached, the program also prints out how many coins the player retrieved.'''
+
+def lever_spot(col, row):
+    coin = 0
+    if col == 1 and row == 2:
+        lever = (input("Pull the lever? (y/n)"))
+        if lever == 'y':
+            coin += 1
+            coin += 1
+            print ("You recieved 1 coin your total is now", coin)
+
+    elif col == 2 and row == 2:
+        lever = (input("Pull the lever? (y/n)"))
+        if lever == 'y':
+            coin += 1
+            print ("You recieved 1 coin your total is now", coin)
+
+    elif col == 2 and row == 3:
+        lever = (input("Pull the lever? (y/n)"))
+        if lever == 'y':
+            coin += 1
+            print ("You recieved 1 coin your total is now", coin)
+
+    elif col == 3 and row == 2:
+        lever = (input("Pull the lever? (y/n)"))
+        if lever == 'y':
+            coin += 1
+            print ("You recieved 1 coin your total is now", coin)
+    return coin
+    
+    
+
 # Constants
 NORTH = 'n'
 EAST = 'e'
@@ -5,17 +51,22 @@ SOUTH = 's'
 WEST = 'w'
 
 
+
 def main():
     # The main program starts here
     victory = False
     row = 1
     col = 1
+    
 
     while not victory:
         valid_directions = find_directions(col, row)
+        pull_lever = lever_spot(col, row, )
+            
         print_directions(valid_directions)
         victory, col, row = play_one_move(col, row, valid_directions)
-    print("Victory!")
+
+    print("Victory! Total coins: ", pull_lever)
 
         
 def find_directions(col, row):
